24ecf34b10ed9d11868bc629a28d2f663185c753
Cedric Hurst <cedric@spantree.net>
2025-10-15 21:24:57 -0500

    chore(formula): update checksums for v0.1.0

--- Formula/fluent-toolkit.rb
+++ Formula/fluent-toolkit.rb
@@ -9,16 +9,16 @@ class FluentToolkit < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/spantree/fluent-toolkit/releases/download/v0.1.0/ftk-darwin-arm64"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_ARM64"
+      sha256 "871b23a5da241e5e955584f7ed76fd1a83ed499c3aa18711f17b2b8d2662e4d2"
     else
       url "https://github.com/spantree/fluent-toolkit/releases/download/v0.1.0/ftk-darwin-x86_64"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_X86_64"
+      sha256 "7d3fb6150b90e79ed042c7293d50a5591fe85e36c2e49600f9f07b1edd70cab8"
     end
   end
 
   on_linux do
     url "https://github.com/spantree/fluent-toolkit/releases/download/v0.1.0/ftk-linux-x86_64"
-    sha256 "REPLACE_WITH_ACTUAL_SHA256_LINUX"
+    sha256 "ff706fc1e28a63c5fac94c3c51c82f3026ea1fec00dfcbc55883b78d3ef1deea"
   end
 
   def install
